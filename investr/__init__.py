56598cda7bf4838881010fb45a80df419d13cdbd
peter-j-schmidt <schmidt.peter.john@gmail.com>
2021-09-19 05:53:06 -0400

    Added db.py and functions to open/ close database.

--- investr/__init__.py
+++ investr/__init__.py
@@ -1,5 +1,26 @@
 import os
 
-import flask from Flask
+from flask import Flask
 
 def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'investr.sqlite'),
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/welcome')
+    def welcome():
+        return 'Welcome!'
+
+    return app
