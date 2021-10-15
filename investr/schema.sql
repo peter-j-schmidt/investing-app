8101f371682e73d7c7573e14239350a168ccab37
peter-j-schmidt <schmidt.peter.john@gmail.com>
2021-10-15 11:40:32 -0400

    Added SQL commands to create tables in sqlite for user, transactions, and stocks owned by a user.

---
+++ investr/schema.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS transactions;
+
+
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+);
+
+CREATE TABLE transactions (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    stock_sympol TEXT NOT NULL,
+    stock_name TEXT NOT NULL,
+    shares INTEGER NOT NULL,
+    price NUMERIC NOT NULL,
+    timestamp TEXT NOT NULL,
+);
+
+CREATE TABLE stocks_owned (
+    index INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    stock_symbol TEXT NOT NULL,
+    stock_name TEXT NOT NULL,
+    total_shares INTEGER NOT NULL,
+)
