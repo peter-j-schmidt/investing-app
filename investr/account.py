1a73c1964b524c38b7c400330cffc66e489b4ee8
peter-j-schmidt <schmidt.peter.john@gmail.com>
2021-10-25 09:36:25 -0400

    Began setting up account Blueprint

--- investr/account.py
+++ investr/account.py
@@ -3,7 +3,7 @@ import functools
 from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from flaskr.db import get_db
+from investr.db import get_db
 
 bp = Blueprint('account', __name__, url_prefix='/account')
 
