c5be356d25ea7d3d761ed8f41d6a60ed107d876f
peter-j-schmidt <schmidt.peter.john@gmail.com>
2021-10-30 11:46:15 -0400

    registered account blueprint

--- investr/__init__.py
+++ investr/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'investr.sqlite'),
@@ -29,4 +30,8 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
+    from . import account
+    app.register_blueprint(account.bp)
+
     return app
+
