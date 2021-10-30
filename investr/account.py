12d563d3aa5ad86ac136a18781e56177f810e56a
peter-j-schmidt <schmidt.peter.john@gmail.com>
2021-10-30 11:46:56 -0400

    updated account blueprint to show home view upon login.

--- investr/account.py
+++ investr/account.py
@@ -8,4 +8,8 @@ from investr.db import get_db
 bp = Blueprint('account', __name__, url_prefix='/account')
 
 
-@bp.route('/portfolio', methods = ['GET', 'POST'])
+@bp.route('/home', methods = ['GET', 'POST'])
+def home():
+    if request.method == "GET":
+
+        return render_template('account/home.html')
